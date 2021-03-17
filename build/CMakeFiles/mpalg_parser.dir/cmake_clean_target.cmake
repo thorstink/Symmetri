b31c8ac0d2a99e1e2937f1318f7bbc590301b50e
Thomas Horstink <thomashorstink@gmail.com>
2021-03-17 18:12:15 +0100

    ignore build and install

--- build/CMakeFiles/mpalg_parser.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libmpalg_parser.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thomas/Projects/petri-net-program
-# Build directory: /home/thomas/Projects/petri-net-program/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
