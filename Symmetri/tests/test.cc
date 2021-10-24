02257c7094946294a8a771e5e602eff72feb918c
Thomas Horstink <thomashorstink@gmail.com>
2021-10-24 11:16:56 +0200

    wip tests

---
+++ Symmetri/tests/test.cc
@@ -0,0 +1,12 @@
+#include "catch/catch.hpp"
+
+unsigned int Factorial(unsigned int number) {
+  return number <= 1 ? number : Factorial(number - 1) * number;
+}
+
+TEST_CASE("Factorials are computed", "[factorial]") {
+  REQUIRE(Factorial(1) == 1);
+  REQUIRE(Factorial(2) == 2);
+  REQUIRE(Factorial(3) == 6);
+  REQUIRE(Factorial(9) == 3628800);
+}
