c35af24f3d98c385bab45f6e1b8e8482dcce3424
Thomas Horstink <thomashorstink@gmail.com>
2024-06-05 21:06:49 +0200

    little refactor

---
+++ symmetri/gui/graph_reducers.h
@@ -0,0 +1,41 @@
+#pragma once
+#define IMGUI_DEFINE_MATH_OPERATORS
+
+#include "imgui.h"
+#include "symmetri/colors.hpp"
+
+void moveView(const ImVec2& d);
+
+void moveNode(bool is_place, size_t idx, const ImVec2& d);
+
+void addNode(bool is_place, ImVec2 pos);
+
+void removeArc(bool is_input, size_t transition_idx, size_t sub_idx);
+
+void addArc(bool is_place, size_t source, size_t target, symmetri::Token color);
+
+void removePlace(size_t idx);
+
+void removeTransition(size_t idx);
+
+void updateTransitionName(const size_t idx, const std::string& d);
+
+void updateTransitionPriority(const size_t idx, const int8_t priority);
+
+void updateArcColor(bool is_input, size_t idx, size_t sub_idx,
+                    const symmetri::Token color);
+
+void updatePlaceName(const size_t idx, const std::string& d);
+
+void setContextMenuActive();
+
+void setContextMenuInactive();
+
+void setSelectedNode(bool is_place, size_t idx);
+
+void resetSelection();
+
+void setSelectedArc(bool is_input, size_t idx, size_t sub_idx);
+
+void renderNodeEntry(bool is_place, const std::string& name, size_t idx,
+                     bool selected);
