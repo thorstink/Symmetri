d2be44edde94ed14a65e25a34615bf3dff0d9c3d
Thomas Horstink <thomashorstink@gmail.com>
2026-06-19 10:31:06 +0200

    clear or append when loading file

--- symmetri/gui/load_file.h
+++ symmetri/gui/load_file.h
@@ -1,4 +1,8 @@
 #pragma once
 #include <filesystem>
 
+// Append the net in `file` to the current net.
 void loadPetriNet(const std::filesystem::path& file);
+
+// Replace the current net with the net in `file`.
+void clearAndloadPetriNet(const std::filesystem::path& file);
